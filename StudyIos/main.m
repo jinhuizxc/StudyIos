d2f132a4a83ce7a23cb516991182f219f902673f
jiangs <js123456789>
2018-01-09 15:12:15 +0800

    创建新的dev分支

--- StudyIos/main.m
+++ StudyIos/main.m
@@ -13,6 +13,7 @@ int main(int argc, char * argv[]) {
     @autoreleasepool {
 //        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
         NSLog(@"学习下log");
+         NSLog(@"创建dev分支");
     }
     return 0;
 }
